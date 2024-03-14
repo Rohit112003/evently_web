85b05995240491df62dfbafc65ffdcfda410e1ea
Rohit Kumar <rohitK79739@gmail.com>
2024-03-14 22:04:50 +0530

    New Commits

--- lib/Database/index.ts
+++ lib/Database/index.ts
@@ -3,6 +3,22 @@ const MONGODB_URI = process.env.MONGODB_URI
 
 let cached = (global as any).mongoose || {conn:null, promise:null};
 
-export const connecToDatabase = ()=>{
+export const connecToDatabase = async  ()=>{
     if(cached.conn) return cached.conn;
+
+    if(!MONGODB_URI) throw new Error("MONGODB_URI not available")
+
+    cached.promise  = cached.promise || mongoose.connect(MONGODB_URI, {
+        dbName:'evently',
+        bufferCommands:false
+    })
+
+    cached.conn = await cached.promise;
+    return cached.conn;
 }
+
+
+
+
+
+//MRC4IhEC5nh1PrEo
